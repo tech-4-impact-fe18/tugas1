11df8e16c6072be584c60b5193d0f6477165e065
BrucelD14 <samuderaduta144@gmail.com>
2022-10-20 13:54:32 +0700

    menambah function untuk validasi login

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
-const inputEmail = document.getElementById('inputEmail');
-const inputPassword = document.getElementById('inputPassword');
+let inputEmail = document.getElementById('inputEmail');
+let inputPassword = document.getElementById('inputPassword'); 
 const loginButton = document.getElementById('loginButton');
 const loginGoogle = document.getElementById('loginGoogle');
 
 
 loginButton.addEventListener('click', (ev) => {
     ev.preventDefault()
-    alert('okee')
+    // console.log(inputEmail.value);
+    // console.log(inputPassword.value);
+    fetch('https://6350b1d078563c1d82c627f2.mockapi.io/persons')
+        .then(response => response.json())
+        .then(response => {
+            for (let i in response) {
+                // console.log(response[i].email);
+                // console.log(response[i].password);
+                if(inputEmail.value == "" || inputPassword.value == "") {
+                    console.log('lengkapi email dan password kamu');
+                    break
+                } else {
+                    if(inputEmail.value == response[i].email && inputPassword.value == response[i].password) {
+                        console.log('okee');
+                        alert('login berhasil');
+                        // location.reload()
+                        break;
+                    } 
+                    // else {
+                    //     console.log('noo');
+                    //     // alert('salah')
+                    // }
+                }
+            }
+            // console.log('akun tidak ada');
+
+        })
 })
 
 
