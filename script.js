1c797f645bc361c92422c929fa9a3f3d4f262513
BrucelD14 <samuderaduta144@gmail.com>
2022-10-20 09:56:16 +0700

    mulai coding js

---
+++ script.js
@@ -0,0 +1,12 @@
+const inputEmail = document.getElementById('inputEmail');
+const inputPassword = document.getElementById('inputPassword');
+const loginButton = document.getElementById('loginButton');
+const loginGoogle = document.getElementById('loginGoogle');
+
+
+loginButton.addEventListener('click', (ev) => {
+    ev.preventDefault()
+    alert('okee')
+})
+
+
